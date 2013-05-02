9fbfd23e8a9f99379b578f18d339d08a33dfc4f0
Oguzhan Gungor <cansingungor@gmail.com>
2013-05-02 17:23:52 +1000

    Xcode Project added

---
+++ DynamicObject/DynamicObject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DynamicObject
+//
+//  Created by Oguzhan Gungor on 2/05/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
